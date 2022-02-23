422343bb88a6a5bb2b91634fb310faa715dce903
Thanh Dep Trai <nvthanh1804@gmail.com>
2022-02-23 09:10:10 +0900

    first commit

---
+++ interfaces/location.ts
@@ -0,0 +1,12 @@
+export interface ILocation {
+  id: number | string;
+  Pro_No: string;
+  Building_No?: number | string;
+  Floor_No?: number | string;
+  Row_Locate?: string | number;
+  No_Locate?: string | number;
+  Shelf?: string | number;
+  Note?: string;
+  Qty?: number | string;
+  identify?: string;
+}
