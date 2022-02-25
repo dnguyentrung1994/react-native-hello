77859b125e5665701a13ba5df02f4bc72ac4d3f2
dnguyentrung1994 <msmsms4656@gmail.com>
2022-02-25 16:29:35 +0900

    fix: style for action buttons

--- screens/BarcodeScan.tsx
+++ screens/BarcodeScan.tsx
@@ -1,6 +1,7 @@
+import { useIsFocused } from '@react-navigation/native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { useEffect, useState } from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import BarcodeMask from 'react-native-barcode-mask';
 import { Button } from 'react-native-elements';
 import { setBarcodeData } from '../redux/location.store';
@@ -13,7 +14,8 @@ const height = Dimensions.get('window').height;
 const viewMinX = (width - finderWidth) / 2;
 const viewMinY = (height - finderHeight) / 2;
 
-export default function BarcodeScanScreen() {
+export default function BarcodeScanScreen({ navigation }: any) {
+  const isFocus = useIsFocused();
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [scanned, setScanned] = useState(false);
   const dispatch = useAppDispatch();
@@ -23,6 +25,11 @@ export default function BarcodeScanScreen() {
       setHasPermission(status === 'granted');
     })();
   }, []);
+  useEffect(() => {
+    return () => {
+      setScanned(false);
+    };
+  }, [isFocus]);
 
   const handleBarCodeScanned = ({ type, data, bounds: { origin } }: any) => {
     const { x, y } = origin;
@@ -40,25 +47,71 @@ export default function BarcodeScanScreen() {
     return <Text>No access to camera</Text>;
   }
   console.log(scanned);
-  return (
+  return isFocus ? (
     <View style={{ flex: 1 }}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-        style={[StyleSheet.absoluteFillObject, styles.barcodeArea]}
-      >
-        <BarcodeMask
-          width={finderWidth - 150}
-          height={finderHeight - 150}
-          edgeColor="#62B1F6"
-          showAnimatedLine={false}
-          outerMaskOpacity={0.7}
-        />
-      </BarCodeScanner>
-      <View style={styles.scanAgainButton}>
-        {scanned && <Button title="Scan Again" onPress={() => setScanned(false)} />}
+      <View style={{ flex: 1 }}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+          style={[StyleSheet.absoluteFillObject, styles.barcodeArea]}
+        >
+          <BarcodeMask
+            width={finderWidth - 150}
+            height={finderHeight - 150}
+            edgeColor="#62B1F6"
+            showAnimatedLine={false}
+            outerMaskOpacity={0.7}
+          />
+        </BarCodeScanner>
       </View>
+
+      {scanned && (
+        <View style={styles.scanAgainArea}>
+          <Button
+            title={'Back'}
+            icon={{
+              name: 'arrow-left',
+              type: 'font-awesome',
+              size: 15,
+              color: 'white',
+            }}
+            buttonStyle={{
+              backgroundColor: 'rgba(244, 132, 42, 0.8)',
+              borderColor: 'transparent',
+              borderWidth: 0,
+              borderRadius: 30,
+            }}
+            containerStyle={{
+              width: '100%',
+              marginVertical: 10,
+            }}
+            onPress={() => navigation.navigate('Location')}
+          />
+          <Button
+            title={'Scan Again'}
+            icon={{
+              name: 'redo',
+              type: 'font-awesome-5',
+              size: 15,
+              color: 'white',
+            }}
+            buttonStyle={{
+              backgroundColor: 'rgba(42, 150, 244, 0.8)',
+              borderColor: 'transparent',
+              borderWidth: 0,
+              borderRadius: 30,
+            }}
+            containerStyle={{
+              width: '100%',
+              marginVertical: 10,
+            }}
+            onPress={() => setScanned(false)}
+          />
+        </View>
+      )}
     </View>
+  ) : (
+    <View></View>
   );
 }
 const styles = StyleSheet.create({
@@ -68,10 +121,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  scanAgainButton: {
+  scanAgainArea: {
     bottom: 0,
-    width: '100%',
+    right: 0,
+    width: '20%',
     display: 'flex',
     position: 'absolute',
+    flexDirection: 'column',
+    marginBottom: 20,
   },
 });
